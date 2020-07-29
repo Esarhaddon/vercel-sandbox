f56d6b7678868c892fb8ee3ffe7566687bbe4a3c
Jackson Dickison <jaksondikison@gmail.com>
2020-07-29 15:12:04 -0700

    try storing env var in a const

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 export default () => {
+  const ref = process.env.VERCEL_GITHUB_COMMIT_REF
+
   return (
     <div>
       <div>hello from index.js!</div>
-      <div>
-        process.env.VERCEL_GITHUB_COMMIT_REF is{" "}
-        {process.env.VERCEL_GITHUB_COMMIT_REF}
-      </div>
+      <div>process.env.VERCEL_GITHUB_COMMIT_REF is {ref}</div>
     </div>
   )
 }
