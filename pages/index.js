e76346b11215897e5269aa99d8c985f7fefa7e00
Jackson Dickison <jaksondikison@gmail.com>
2020-07-29 15:18:10 -0700

    use NEXT_PUBLIC_ prefix

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 export default () => {
-  const ref = process.env.VERCEL_GITHUB_COMMIT_REF
-
   return (
     <div>
       <div>hello from index.js!</div>
-      <div>process.env.VERCEL_GITHUB_COMMIT_REF is {ref}</div>
+      <div>
+        process.env.VERCEL_GITHUB_COMMIT_REF is{" "}
+        {process.env.NEXT_PUBLIC_COMMIT_REF}
+      </div>
     </div>
   )
 }
