e6e6ba9249dbe98d72099a7b780dd9c299b19d36
Jackson Dickison <jaksondikison@gmail.com>
2020-07-29 14:34:22 -0700

    show branch env var

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
-export default () => <div>hello from index.js!</div>
+export default () => {
+  return (
+    <div>
+      <div>hello from index.js!</div>
+      <div>
+        process.env.VERCEL_GITHUB_COMMIT_REF is{" "}
+        {process.env.VERCEL_GITHUB_COMMIT_REF}
+      </div>
+    </div>
+  )
+}
