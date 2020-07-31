0effb6e9eae4639fde54ba4360b1ca855b2947aa
Jackson Dickison <jaksondikison@gmail.com>
2020-07-31 09:01:38 -0700

    fix message

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,7 @@ export default () => {
   return (
     <div>
       <div>hello from index.js!</div>
-      <div>
-        process.env.VERCEL_GITHUB_COMMIT_REF is{" "}
-        {process.env.NEXT_PUBLIC_COMMIT_REF}
-      </div>
+      <div>commit ref is {process.env.NEXT_PUBLIC_COMMIT_REF}</div>
     </div>
   )
 }
