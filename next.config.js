2aab70601ae010af19e8a14b0086e7eba479cc7c
Jackson Dickison <jaksondikison@gmail.com>
2020-07-31 09:13:51 -0700

    use unprefixed env var

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   env: {
     NEXT_PUBLIC_COMMIT_REF: process.env.VERCEL_GITHUB_COMMIT_REF,
+    COMMIT_REF: process.env.VERCEL_GITHUB_COMMIT_REF,
   },
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default () => {
   return (
     <div>
       <div>hello from index.js!</div>
-      <div>commit ref is {process.env.NEXT_PUBLIC_COMMIT_REF}</div>
+      <div>commit ref is {process.env.COMMIT_REF}</div>
     </div>
   )
 }
